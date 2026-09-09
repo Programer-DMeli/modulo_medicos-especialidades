56ab8746a8003d9dd7af60fd4926322a782505fc
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-09 14:01:17 -0500

    first commit

---
+++ src/main/java/com/tecsup/medicos_especialidades04/MedicosEspecialidades04Application.java
@@ -0,0 +1,13 @@
+package com.tecsup.medicos_especialidades04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicosEspecialidades04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicosEspecialidades04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecsup/medicos_especialidades04/MedicosEspecialidades04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.medicos_especialidades04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicosEspecialidades04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4fb87ec4f8a3be8d0849033fbaaa85eb3f2c50b5
Angie-67 <angievasquezgg@gmail.com>
2026-09-11 23:47:58 -0500

    Guardar cambios preeliminares de medico service

--- src/main/java/com/tecsup/medicos_especialidades04/Service/MedicoService.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Service/MedicoService.java
@@ -1,4 +1,14 @@
 package com.tecsup.medicos_especialidades04.Service;
 
+import com.tecsup.medicos_especialidades04.Repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MedicoService {
+
+    @Autowired
+    private MedicoRepository repo;
+
+    public class List<MedicoModel> listar()
 }

cd1e691e2a0e7d3b81a0b1c6213379d77b8b13c7
Angie-67 <angievasquezgg@gmail.com>
2026-09-12 00:09:16 -0500

    Implemente el controller y service de medico

--- src/main/java/com/tecsup/medicos_especialidades04/Controller/MedicoController.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Controller/MedicoController.java
@@ -1,5 +1,26 @@
 package com.tecsup.medicos_especialidades04.Controller;
 
-public class MedicoController {
+import com.tecsup.medicos_especialidades04.Model.MedicoModel;
+import com.tecsup.medicos_especialidades04.Service.MedicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class MedicoController {
+    @Autowired
+    private MedicoService service;
+
+    @GetMapping
+    public List<MedicoModel> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicoModel> guardar(@RequestBody MedicoModel medico) {
+        return ResponseEntity.status(201).body(service.guardar(medico));
+    }
 }
--- src/main/java/com/tecsup/medicos_especialidades04/Service/MedicoService.java
+++ src/main/java/com/tecsup/medicos_especialidades04/Service/MedicoService.java
@@ -1,8 +1,11 @@
 package com.tecsup.medicos_especialidades04.Service;
 
+import com.tecsup.medicos_especialidades04.Model.MedicoModel;
 import com.tecsup.medicos_especialidades04.Repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MedicoService {
@@ -10,5 +13,11 @@ public class MedicoService {
     @Autowired
     private MedicoRepository repo;
 
-    public class List<MedicoModel> listar()
+    public List<MedicoModel> listar() {
+        return repo.findAll();
+    }
+
+    public MedicoModel guardar(MedicoModel medico) {
+        return repo.save(medico);
+    }
 }
